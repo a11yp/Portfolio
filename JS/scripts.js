7e8eb2c9a2299c0dee48f9213ab07a45ec8159b9
a11yp <allyplatt1@gmail.com>
2021-12-28 14:16:46 +0000

    Changes for code quality

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,11 @@
 console.log("hello, welcome to my portfolio site")
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle') {.,ljo
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
+
 }
